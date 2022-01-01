01fca19e6d2afdd52312b884d6bfca4831ab86f4
Lorenzo Manicardi <Chico@192.168.1.56>
2022-01-01 22:15:55 +0100

    names for pages in the browser

--- frontend_vue/src/views/Product.vue
+++ frontend_vue/src/views/Product.vue
@@ -50,7 +50,8 @@ export default {
             await axios
                 .get(`/api/v1/products/${category_slug}/${product_slug}`)
                 .then(response => {
-                    this.product = response.data  
+                    this.product = response.data
+                    document.title = this.product.name + " - ManicardiShop"
                 })
                 .catch(error => {
                     console.log(error)
