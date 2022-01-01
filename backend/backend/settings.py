9c473647516d10e85b77363db78b45c1489deddb
Lorenzo Manicardi <Chico@192.168.1.56>
2022-01-01 16:49:49 +0100

    created a simple frontpage and displayed latestproducts

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -44,10 +44,6 @@ INSTALLED_APPS = [
     'product',
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:8000",
-]
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -58,6 +54,11 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:8080',
+    'http://127.0.0.1:8000',
+)
 
 ROOT_URLCONF = 'backend.urls'
 
