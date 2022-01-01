95f394346d1c1af4aca283af5c26fed25df84d6a
Lorenzo Manicardi <Chico@192.168.1.56>
2022-01-01 16:55:10 +0100

    show 4 latest products in home instead of 3

--- backend/product/views.py
+++ backend/product/views.py
@@ -6,7 +6,7 @@ from .serializers import ProductSerializer
 
 class  LatestProductsList(APIView):
     def get(self, request, format=None):
-        products = Product.objects.all()[0:3]
+        products = Product.objects.all()[0:4]
         serializer = ProductSerializer(products, many=True)
         return Response(serializer.data)
 
