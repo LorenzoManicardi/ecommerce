5d41fe5fcfa36a1c6e02f408afa6c18dbe238d7a
Lorenzo Manicardi <Chico@192.168.1.56>
2022-01-01 21:25:40 +0100

    add to cart funcionality

--- frontend_vue/src/views/Product.vue
+++ frontend_vue/src/views/Product.vue
@@ -9,14 +9,14 @@
                 <p>{{ product.description }}</p>
             </div>
             <div class="column is-3">
-                <h2 class="subtitle">Information</h2>
+                <h2 class="subtitle">Infos</h2>
                 <p><strong>Price: </strong>${{ product.price }}</p>
                 <div class="field has-addons mt-6">
                     <div class="control">
                         <input type="number" class="input" min="1" v-model="quantity">
                     </div>
                     <div class="control">
-                        <a class="button is-dark">Add to cart</a>
+                        <a class="button is-dark" @click="addToCart">Add to cart</a>
                     </div>
                 </div>
             </div>
@@ -27,6 +27,7 @@
 
 <script>
 import axios from 'axios'
+import { toast } from 'bulma-toast'
 
 export default {
     name: 'Product',
@@ -52,6 +53,27 @@ export default {
                 .catch(error => {
                     console.log(error)
                 })
+        },
+        addToCart() {
+            if (isNaN(this.quantity) || this.quantity < 1) {
+                this.quantity = 1
+            } 
+
+            const item = {
+                product: this.product,
+                quantity: this.quantity,
+            }
+
+            this.$store.commit('addToCart', item)
+
+            toast({
+                message: 'The product was added to the cart',
+                type: 'is-success',
+                dismissible: true,
+                pauseOnHover: true,
+                duration: 2000,
+                position: 'bottom-right',
+            })
         }
     }
 }
